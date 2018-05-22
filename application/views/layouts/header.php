5b6ac6ec2c37a56defbe94b80e90d6fea04703e9
Berta Fly <inovykov@e1r4p4.unit.ua>
2018-05-22 18:43:53 +0300

    all pages max height

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -9,14 +9,14 @@
                 About
             </a>
             <?php if (array_key_exists('authorizedUser', $_SESSION)): ?>
-                <a href="user/cabinet" class="login">
+                <a href="http://localhost:8070/user/cabinet" class="login">
                     <?php echo $_SESSION['authorizedUser']?>
                 </a>
                 <a href="logout">
                     Logout
                 </a>
             <?php else: ?>
-                <a href="user/login">
+                <a href="http://localhost:8070/user/login">
                     Log/Sign in
                 </a>
             <?php endif; ?>
